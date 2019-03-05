ba6840900db1273e9b47309eba95ad5895989fa1
JessicaYunga44 <43676614+JessicaYunga44@users.noreply.github.com>
2019-03-05 09:38:40 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#paying").click(function() {
+    $("#answer").text("Of Course");
+    $("img").attr("src","https://www.ozepac.com.au/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/c/bcb-t-t_edited.png");
+    $("img").show ();
+    $("#answer").text("Of Course");
+});
+$("#Notpaying").click(function() {
+    $("#answer").text("~ gives a tip to the cashier~ No thank you");
+});
